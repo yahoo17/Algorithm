ce84a36bbad2d423e1d95550baf15bc7fa86495f
yahoo17 <55846951+yahoo17@users.noreply.github.com>
2020-04-13 21:01:09 +0800

    1

---
+++ 百练-dp-动态规划/百练-dp-动态规划.cpp
@@ -0,0 +1,51 @@
+﻿// 百练-dp-动态规划.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+//动态规划
+#include <iostream>
+#include <vector>
+using namespace std;
+#define N 101
+int a[N][N];
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+int result(int r, int c,int n)
+{
+    if (r == n)
+        return a[r][c];
+    else
+        return a[r][c]+max(result(r + 1, c,n), result(r + 1, c + 1,n));
+    
+}
+int main()
+{
+  /*  int group;
+    cin >> group;
+    vector <int> res;
+    for (int i = 0; i < group; i++)
+    {*/
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+                cin >> a[i][j];
+        }
+        
+        cout << result(1, 1, n);
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+}
+
---
+++ 百练-生理周期/百练-生理周期.cpp
@@ -0,0 +1,30 @@
+﻿// 百练-生理周期.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+//暴力解法
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    for (int i = d; i < 21252; i++)
+    {
+        if ((i - a) % 23 == 0 && (i - b) % 28 == 0 && (i - c) % 33 == 0)
+            cout << i << endl;
+    }
+   
+}
+
+//int main()
+//{
+//	int p, e, i, d;
+//
+//	cin >> p >> e >> i >> d;
+//	int now=d;
+//	for ( ; (now - p) % 23 != 0; now++);
+//	for (; (now - e) % 28 != 0; now+=23);
+//	for (; ((now - i) % 33 != 0)&&now<=21252; now += 23 * 28);
+//	cout << now-d;
+//}
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜
