35ae24f0bdc67684bcce4b046202e8ae9b2718e7
yahoo17 <55846951+yahoo17@users.noreply.github.com>
2020-04-14 20:22:13 +0800

    1

---
+++ leetcode-反转链表/leetcode-反转链表.cpp
@@ -0,0 +1,23 @@
+﻿// leetcode-反转链表.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+int main()
+{
+    std::cout << "Hello World!\n";
+    stack<int> temp;
+	ListNode *
+	while (true)
+	{
+
+	}
+}
