a365e7e26e491a9004ed3dfa0b148076d7c6b92a
yahoo17 <55846951+yahoo17@users.noreply.github.com>
2020-04-15 20:25:57 +0800

    1

---
+++ testForVector/testForVector.cpp
@@ -0,0 +1,17 @@
+﻿// testForVector.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+using namespace std;
+int main()
+{
+    vector<int> i1;
+    i1.push_back(1);
+    i1.push_back(3);
+    vector<int>::iterator it ;
+    cout << sizeof(it)/sizeof(int)<<endl;
+    cout << typeid(it).name()<<endl;
+    
+}
